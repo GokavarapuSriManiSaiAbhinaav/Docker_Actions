f14bc17c3b6e62390f67b86c38ccf80625402cc7
ABHINAV <2300031133@gmail.com>
2025-10-16 09:30:55 +0530

    updated

--- onlineartgallery/src/config.js
+++ onlineartgallery/src/config.js
@@ -1,7 +1,7 @@
 const config = {
     //url: "http://localhost:2030/apionlineart"
     //url: "http://localhost:3025"
-    url: "http://localhost:2080"
+    url: "http://localhost:2025"
 }
 
 export default config;
